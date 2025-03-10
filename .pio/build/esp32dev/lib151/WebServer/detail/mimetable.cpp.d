b1e8ce3a3eb9f75056fab85be2184dbbb16f2a61
Rohan Batra <rohanbatra.in@gmail.com>
2025-03-10 19:46:41 +0530

    ok

--- .pio/build/esp32dev/lib151/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/lib151/WebServer/detail/mimetable.cpp.o: \
- C:/Users/rohan/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- C:/Users/rohan/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- C:/Users/rohan/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
--- .pio/build/esp32dev/libb0c/WebSockets/libb64/cdecode.c.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32dev/libb0c/WebSockets/libb64/cdecode.c.o: \
- .pio/libdeps/esp32dev/WebSockets/src/libb64/cdecode.c
